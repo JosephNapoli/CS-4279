5132557048ad655c648f16df65411d9554035022
Sam Feifer <sam.feifer@gmail.com>
2023-04-18 11:32:15 -0500

    might fix deployment with oauth

--- golfapp/src/aws-exports.js
+++ golfapp/src/aws-exports.js
@@ -20,8 +20,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000",
-        "redirectSignOut": "http://localhost:3000",
+        "redirectSignIn": "http://localhost:3000,https://master.dvfbedu6tplu.amplifyapp.com/",
+        "redirectSignOut": "http://localhost:3000,https://master.dvfbedu6tplu.amplifyapp.com/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
